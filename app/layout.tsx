9dacb440a858e9e3da3fb3b1d8e270f88976902f
mags0ft <magnusschlinsog@gmail.com>
2025-01-29 21:38:53 +0100

    add Chakra UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Provider } from "@/components/ui/provider"
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { Geist } from "next/font/google";
@@ -34,35 +35,37 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>litictactoe</Link>
+          <Provider>
+            <main className="min-h-screen flex flex-col items-center">
+              <div className="flex-1 w-full flex flex-col gap-20 items-center">
+                <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                  <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                    <div className="flex gap-5 items-center font-semibold">
+                      <Link href={"/"}>litictactoe</Link>
+                    </div>
+                    <HeaderAuth />
                   </div>
-                  <HeaderAuth />
+                </nav>
+                <div className="flex flex-col gap-20 max-w-5xl p-5">
+                  {children}
                 </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Visit this site on{" "}
-                  <a
-                    href="https://github.com/mags0ft/litictactoe"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                  >
-                    GitHub
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
+                <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+                  <p>
+                    Visit this site on{" "}
+                    <a
+                      href="https://github.com/mags0ft/litictactoe"
+                      target="_blank"
+                      className="font-bold hover:underline"
+                    >
+                      GitHub
+                    </a>
+                  </p>
+                  <ThemeSwitcher />
+                </footer>
+              </div>
+            </main>
+          </Provider>
         </ThemeProvider>
       </body>
     </html>
